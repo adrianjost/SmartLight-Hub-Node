92b802e9465d9d46b01465ca304d33b0bbab2a1a
adrianjost <22987140+adrianjost@users.noreply.github.com>
2021-05-01 10:52:38 +0200

    ✨ implement hub login

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ const polka = require("polka");
 const firebase = require("firebase/app");
 require("firebase/auth");
 require("firebase/firestore");
-const WebSocket = require('ws');
+const WebSocket = require("ws");
+const fs = require("fs").promises;
+const { urlencoded } = require("body-parser");
+
+const LocalStorage = require("node-localstorage").LocalStorage;
+localStorage = new LocalStorage("./localstorage");
+
+const styles = `<link rel="stylesheet" href="/styles.css">`;
 
 const firebaseConfig = {
   apiKey: "AIzaSyDJ17cuZ4P1YzSTOWtU_WqOKMloaqg7x_Q",
@@ -19,69 +26,86 @@ const db = firebase.firestore();
 
 var unitsRef = db.collection("units");
 
-let credentials;
+let credential = null;
 
 const hex2rgb = (hexColor) => {
-	// remove leading #
-	if (hexColor.length === 7 || hexColor.length === 4) {
-		hexColor = hexColor.substr(1);
-	}
-	// convert 3 digit to 6 digit color
-	if (hexColor.length === 3) {
-		hexColor =
-			hexColor[0] +
-			hexColor[0] +
-			hexColor[1] +
-			hexColor[1] +
-			hexColor[2] +
-			hexColor[2];
-	}
-	const bigint = parseInt(hexColor, 16);
-	const r = (bigint >> 16) & 255;
-	const g = (bigint >> 8) & 255;
-	const b = bigint & 255;
-	return { r: r, g: g, b: b };
+  // remove leading #
+  if (hexColor.length === 7 || hexColor.length === 4) {
+    hexColor = hexColor.substr(1);
+  }
+  // convert 3 digit to 6 digit color
+  if (hexColor.length === 3) {
+    hexColor =
+      hexColor[0] +
+      hexColor[0] +
+      hexColor[1] +
+      hexColor[1] +
+      hexColor[2] +
+      hexColor[2];
+  }
+  const bigint = parseInt(hexColor, 16);
+  const r = (bigint >> 16) & 255;
+  const g = (bigint >> 8) & 255;
+  const b = bigint & 255;
+  return { r: r, g: g, b: b };
 };
 
 async function updateLocalUnit(unit) {
-  console.log(`connecting to ${unit.hostname}`)
-  const ws =   new WebSocket(`ws://${unit.hostname}`, {
-    perMessageDeflate: false
+  console.log(`connecting to ${unit.hostname || unit.ip}`);
+  const ws = new WebSocket(`ws://${unit.hostname || unit.ip}`, {
+    perMessageDeflate: false,
   });
-  ws.on('open', function open() {
-    console.log("connected!")
-    const {r, b } = hex2rgb(unit.state.color);
-    console.log(unit.state.color, r,b)
-    ws.send(JSON.stringify(
-      {
-        "action": "SET /output/channel",
-        "data": [r, b]
-      }
-    ));
-    ws.close()
+  ws.on("open", function open() {
+    console.log("connected!");
+    const { r, b } = hex2rgb(unit.state.color);
+    console.log(unit.state.color, r, b);
+    ws.send(
+      JSON.stringify({
+        action: "SET /output/channel",
+        data: [r, b],
+      })
+    );
+    ws.close();
   });
 }
 
 async function init() {
   try {
-    // await firebase.auth().createUserWithEmailAndPassword("adrian.aus.berlin+firebase-node-demo@gmail.com", "mySuperSecretKey123")
-    credentials = await firebase
+    const savedLogin = localStorage.getItem("credential");
+    if (savedLogin === null) {
+      console.log("No saved credential found");
+      return;
+    }
+    console.log("saved loginData", savedLogin);
+    loginData = JSON.parse(savedLogin);
+    credential = await firebase
       .auth()
-      .signInWithEmailAndPassword(
-        "adrian.aus.berlin+firebase-node-demo@gmail.com",
-        "mySuperSecretKey123"
-      );
+      .signInWithEmailAndPassword(loginData.email, loginData.password);
 
-    console.log(credentials.user.uid);
-
-    const doc = await unitsRef.doc("0X5F4qjkrFbeOMTzrLT8").get();
-    console.log(doc.data());
+    console.log(credential.user.uid);
 
     await unitsRef
-      .where("allowedUsers", "array-contains", credentials.user.uid)
+      .where("allowedUsers", "array-contains", credential.user.uid)
+      .where("type", "==", "LAMP")
       .onSnapshot((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          updateLocalUnit(doc.data())
+        querySnapshot.docChanges().forEach((change) => {
+          if (change.type === "added") {
+            console.log("added: ", change.doc.data());
+          }
+          if (change.type === "modified") {
+            console.log("modified: ", change.doc.data());
+            try {
+              const unit = change.doc.data();
+              if (unit.type === "LAMP") {
+                updateLocalUnit(unit);
+              }
+            } catch (error) {
+              console.error(error);
+            }
+          }
+          if (change.type === "removed") {
+            console.log("removed: ", change.doc.data());
+          }
         });
       });
   } catch (error) {
@@ -89,23 +113,77 @@ async function init() {
   }
 }
 
-let initialized = false;
 init();
 
-async function one(req, res, next) {
-  req.hello = "world";
-  if (!initialized) {
-    await init();
-    initialized = true;
-  }
-  next();
-}
-
 polka()
-  .use(one)
-  .get("/users/:id", (req, res) => {
-    console.log(`~> Hello, ${req.hello}`);
-    res.end(`User: ${req.params.id}`);
+  .use(urlencoded({ extended: false }))
+  .use(function (req, res, next) {
+    res.redirect = (location) => {
+      let str = `Redirecting to ${location}`;
+      res.writeHead(302, {
+        Location: location,
+        "Content-Type": "text/plain; charset=utf-8",
+        "Content-Length": str.length,
+      });
+      res.end(str);
+    };
+    next();
+  })
+  .get("/", async (req, res) => {
+    if (credential === null) {
+      const file = await fs.readFile("./login.html");
+      res.end(file);
+      return;
+    }
+    res.writeHead(200, {
+      "Content-Type": "text/html; charset=utf-8",
+    });
+    res.end(`${styles}
+    <p style="font-size: 3em;">🎉 🎉 🎉</p>
+    <p>🔓 You are logged in as ${credential.user.email}. 🔓</p>
+    <p>Click <a href="/logout">here</a> to logout.</p>
+    <p style="font-size: 3em;">🎉 🎉 🎉</p>
+    `);
+  })
+  .post("/", async (req, res) => {
+    console.log("BODY:", JSON.stringify(req.body));
+    const { email, password } = req.body;
+    try {
+      credential = await firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password);
+    } catch (error) {
+      console.error(error.code, error.message);
+      res.end(error.message);
+    }
+    localStorage.setItem(
+      "credential",
+      JSON.stringify({
+        email,
+        password,
+      })
+    );
+    init();
+    res.redirect("/");
+  })
+  .get("/logout", async (req, res) => {
+    localStorage.clear();
+    credential = null;
+    await firebase.auth().signOut();
+    res.writeHead(200, {
+      "Content-Type": "text/html; charset=utf-8",
+    });
+    res.end(`
+    <meta http-equiv="refresh" content="5; URL=/">
+    ${styles}
+    <p>🔒 You have been logged out. 🔒</p>
+    <p>Click <a href="/">here</a> to login again.</p>`);
+  })
+  .get("/styles.css", async (req, res) => {
+    res.writeHead(200, {
+      "Content-Type": "text/css; charset=utf-8",
+    });
+    res.end(await fs.readFile("./styles.css"));
   })
   .listen(3000, (err) => {
     if (err) throw err;
